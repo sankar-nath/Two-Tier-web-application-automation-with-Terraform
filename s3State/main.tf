c52be48038ab8cf1ee6b2a54ad72ddd930576ae4
Sankarnath Suresh <sankarnathsuresh@gmail.com>
2022-12-22 14:58:53 -0500

    Fix errors

--- s3State/main.tf
+++ s3State/main.tf
@@ -1,20 +1,19 @@
 #declaring the global variables 
-module "globalvars" {
+module "globalVars" {
   source = "../modules/globalVars"
 }
 
 resource "aws_s3_bucket" "s3Buckets" {
-  for_each      = toset(var.bucket_name)
+  for_each      = toset(var.bucketName)
   bucket        = each.key
   force_destroy = true
-
   tags = module.globalvars.default_tags
 }
 
 
 #Versioning the Buckets
 resource "aws_s3_bucket_versioning" "s3BucketVersion" {
-  for_each = toset(var.bucket_name)
+  for_each = toset(var.bucketName)
   bucket   = each.key
 
   versioning_configuration {
@@ -25,7 +24,7 @@ resource "aws_s3_bucket_versioning" "s3BucketVersion" {
 
 #Blocking public access
 resource "aws_s3_bucket_public_access_block" "bucketAccessPolicy" {
-  for_each = toset(var.bucket_name)
+  for_each = toset(var.bucketName)
   bucket   = each.key
 
   block_public_acls       = true
